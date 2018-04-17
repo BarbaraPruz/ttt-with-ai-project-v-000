22fd893a3d380e70b9aa919afa782fee67983668
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-17 02:55:05 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,3 @@
-require_relative "../config/environment.rb"
 class Game
   WIN_COMBINATIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -22,15 +21,14 @@ class Game
   end
 
   def over?
-    @board.full? || won?
+    won? || draw?
   end
 
   def won?
-    # f!@* up test: this code will detect the 1st win on the board
+    # f!@*ed up test: this code will detect the 1st win on the board
     # but test gives board with 2 wins.  the first X win is the
     # expected result.   Why would/How can we have a board with 2 wins?
     # WIN_COMBINATIONS.detect do | combo |
-    #  binding.pry
     #  @board.cells[combo[0]]!=" " &&
     #  @board.cells[combo[0]]==@board.cells[combo[1]]&&
     #  @board.cells[combo[0]]==@board.cells[combo[2]]
@@ -40,11 +38,11 @@ class Game
       @board.cells[combo[0]]==@board.cells[combo[1]]&&
       @board.cells[combo[0]]==@board.cells[combo[2]]
     end
-    winners.find { | win | board.cells[win[0]]==player_1.token} || winners.find { | win | board.cells[win[0]]==player_2.token}
+    winners.find { | win | @board.cells[win[0]]==@player_1.token} || winners.find { | win | @board.cells[win[0]]==@player_2.token}
   end
 
   def draw?
-     !won? && over?
+    @board.full? && !won?
   end
 
   def winner
@@ -54,18 +52,22 @@ class Game
 
   def turn
     next_move = -1
-    until board.valid_move?(next_move) do
+    until @board.valid_move?(next_move) do
       next_move = current_player.move(@board).to_i
     end
-    board.update(next_move,current_player)
+    @board.update(next_move,current_player)
   end
 
   def play
+    # over? will check for won or draw (full and not won).  This
+    # results in extra calls to won? but is required for test specs.
+    # All we really need is !won? and !full but spec requires both
+    # draw? and over? are called each time through the loop.
     while !over? do
-      board.display
+      @board.display
       turn
     end
-    board.display
+    @board.display
     if draw?
       puts "Cat's Game!"
     else

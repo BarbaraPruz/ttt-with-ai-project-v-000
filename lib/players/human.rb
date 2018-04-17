afcdc69ada157def67753ede177f7e0a48d9464a
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-17 02:45:09 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -4,7 +4,7 @@ module Players
     def move (board)
       puts "Next Move?  (Enter 1..9)"
       # ideally, we would like to change for "position" to index and integer here,
-      # but tests require returning position (1..9) as string
+      # but tests (and board interface) require returning position (1..9) as string
       gets.strip
     end
   end
